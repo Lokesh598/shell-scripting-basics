b38ca284f05eafee2188f9683c8521f7e5734578
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-03-29 22:34:52 +0530

    Building username and password interface

---
+++ 5. loginInterface.sh
@@ -0,0 +1,6 @@
+read -p "Username: " username
+
+read -sp "Password: " password
+# s stands for silent means it will not shows us what we are typing in and p for placeholder
+echo "Your username is: $username"
+echo "your password is: $password"
