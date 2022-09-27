d256034b0d048ca37d87e342e8eefec6b2250d22
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-09-27 12:08:37 +0530

    Create ifelseCheckNo.sh

---
+++ ifelseCheckNo.sh
@@ -0,0 +1,13 @@
+echo Enter the first Number
+read x
+echo Enter the second Number
+read y
+
+if [$x -gt $y]
+then 
+    echo first no is greater
+elif then $y -gt $x
+then 
+    echo second no. is greater
+else
+    echo both are equal
